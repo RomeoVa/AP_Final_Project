067852d705ffe09426be1379011352c1dc26d34f
maupeon <maupeon@gmail.com>
2019-12-07 12:04:16 -0600

    Some updates in graphics

---
+++ graphics.h
@@ -0,0 +1,18 @@
+#ifndef GRAPHICS_H
+#define GRAPHICS_H
+
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include "colors.h"
+#include "uno_functions.h"
+
+void renderWindow(vector<pair <int, int> > *  hand, pair <int,int> * current_card,vector<pair <string, int> > *  players,int turn);
+std::vector<sf::RectangleShape> createRectangles(vector<pair <int, int> > *  hand);
+sf::Color getRectangleColor(int color);
+sf::RectangleShape createCurrentRectangle(pair <int,int> *current_card);
+pair<int,int> getCoords(int player);
+pair<int,int> getTurnCoords(int turn);
+string getArrowTurn(int turn);
+string getSpecialCard(string number);
+
+#endif  
