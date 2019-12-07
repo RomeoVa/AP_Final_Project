42ea00d2bf5f1d8349e5c179530d1118e5dbf953
Mauricio <maupeon@gmail.com>
2019-12-07 10:35:16 -0600

    Added better graphics

--- test_SFML.cpp
+++ test_SFML.cpp
@@ -10,25 +10,21 @@ using namespace std;
 
 void renderWindow(vector<pair <int, int> > *  hand, pair <int,int> * current_card,vector<pair <string, int> > *  players,int turn);
 std::vector<sf::RectangleShape> createRectangles(vector<pair <int, int> > *  hand);
-std::vector<sf::Text> createTexts(vector<pair <int, int> > *  hand);
 sf::Color getRectangleColor(int color);
 sf::RectangleShape createCurrentRectangle(pair <int,int> *current_card);
 pair<int,int> getCoords(int player);
 pair<int,int> getTurnCoords(int turn);
-
-
+string getArrowTurn(int turn);
 
 
 int main()
 {
     vector<pair <int, int> >  hand;
     char cards[BUFFER_SIZE];
-
     std::vector<sf::RectangleShape> rectangles;
     std::vector<sf::Text> texts;
     std::vector<pair<string,int>> players;
     sf::RectangleShape  *current;
-    //sf::Text texts[MAX];
     sf::Text *current_text;
     pair <int,int> current_card;
 
@@ -39,22 +35,12 @@ int main()
     players.push_back(make_pair("Pablo",3));
     players.push_back(make_pair("Gil",5));
 
-
-
     hand = player_hand();
-    //vectorToString(&hand,cards);
-
-    //cout<<"CARTASR"<< cards<<endl;
-
-    //createVisualCards(&hand);
 
     for (int i = 0; i<hand.size(); i++) 
         cout << hand[i].first << ":"<< getColor(&hand[i])<<"   "; 
 
-    //rectangles = createRectangles(&hand);
-    //texts = createTexts(&hand);
-    //createVisualCards(cards);
-    renderWindow(&hand,&current_card, &players,1);
+    renderWindow(&hand,&current_card, &players,0);
 
 
     return 0;
@@ -68,33 +54,29 @@ void renderWindow(vector<pair <int, int> > *  hand, pair <int,int> * current_car
     sf::Text current_text, turn_text;
     sf::RenderWindow window(sf::VideoMode(1000, 600), "UNO++");
     std::vector<sf::Text> names_texts;
-    names_texts.resize(players->size());
-
-    int x_text = 25;
     sf::Font font;
     string number, current_number,name,turn_string;
-    font.loadFromFile("arial.ttf");
+    int x, y,x_turn,y_turn;
+    int x_text = 25;
 
+    window.setPosition(sf::Vector2i(0,20));
+    names_texts.resize(players->size());
+    font.loadFromFile("arial.ttf");
     rectangles = createRectangles(hand);
     current_rectangle = createCurrentRectangle(current_card);
-    //texts = createTexts(hand);
-
     texts.resize(hand->size());
-    //cout<<"tamano"<<(*rectangles)[0]<<endl;
+
     for(int i = 0;i < hand->size();i++)
     {
         number = to_string(hand->at(i).first);
-        //texts.push_back(text);
         cout<<"ENTRA AL FOR del text"<<i<<endl;
         texts[i].setPosition(sf::Vector2f(x_text,50));
         texts[i].setFont(font);
         texts[i].setCharacterSize(20);
         texts[i].setString(number);
         x_text+= 50;
-        //cout<<"Sale del FOR"<<i<<endl;
     }
     
-    int x, y,x_turn,y_turn;
     for(int i = 0;i < players->size();i++)
     {
         x = getCoords(i).first;
@@ -102,17 +84,12 @@ void renderWindow(vector<pair <int, int> > *  hand, pair <int,int> * current_car
         name = players->at(i).first;
         name+=":\n";
         name+= to_string( players->at(i).second);
-        //texts.push_back(text);
         cout<<"ENTRA AL FOR del nombre"<<i<<endl;
         names_texts[i].setPosition(sf::Vector2f(x,y));
         names_texts[i].setFont(font);
         names_texts[i].setCharacterSize(20);
         names_texts[i].setString(name);
-        //x_text+= 50;
-        //cout<<"Sale del FOR"<<i<<endl;
     }
-
-   
 
     current_number = to_string(current_card->first);
     current_text.setPosition(sf::Vector2f(500,300));
@@ -125,10 +102,8 @@ void renderWindow(vector<pair <int, int> > *  hand, pair <int,int> * current_car
     turn_text.setPosition(sf::Vector2f(x_turn,y_turn));
     turn_text.setFont(font);
     turn_text.setCharacterSize(20);
-    turn_text.setString("->");
-    
-    cout<<"ENTRA A LA FUNCION"<<endl;
-    
+    turn_text.setString(getArrowTurn(turn));
+
     while (window.isOpen())
     {
         sf::Event event;
@@ -140,30 +115,20 @@ void renderWindow(vector<pair <int, int> > *  hand, pair <int,int> * current_car
 
         sf::Color darkGreyGreen(10, 30, 10, 255);
         window.clear(darkGreyGreen);
-
         window.draw(current_rectangle);
         window.draw(current_text);
         window.draw(turn_text);
 
-
         for(int i = 0;i < hand->size();i++)
         {
-            //cout<<"ENTRA AL ARREGLO"<<endl;
-            //window.draw(texts[i]);
-            //cout << texts[i].getString().toAnsiString() << endl;
             window.draw(rectangles[i]);
             window.draw(texts[i]);
         }
 
-          for(int i = 0;i < players->size();i++)
-        {
-            //cout<<"ENTRA AL ARREGLO"<<endl;
-            //window.draw(texts[i]);
-            //cout << texts[i].getString().toAnsiString() << endl;
-           
+        for(int i = 0;i < players->size();i++)
+        {           
             window.draw(names_texts[i]);
         }
-
         window.display();
     }
 }
@@ -173,28 +138,17 @@ std::vector<sf::RectangleShape> createRectangles(vector<pair <int, int> > *  han
     std::vector<sf::RectangleShape> rectangles;
     sf::RectangleShape rectangle;
      int x = 10;
-    int y = 10;
-    int x_text = 25;
-    int y_text = 100;
     rectangles.resize(hand->size());
-    //cout<<"tamano"<<(*rectangles)[0]<<endl;
     for(int i = 0;i < hand->size();i++)
     {
-        //rectangles.push_back(rectangle);
-        //cout<<"ENTRA AL FOR "<<i<<endl;
         rectangles[i].setPosition(sf::Vector2f(x,10));
         rectangles[i].setSize(sf::Vector2f(50,100));
         rectangles[i].setFillColor(getRectangleColor(hand->at(i).second));
         rectangles[i].setOutlineColor(sf::Color(255,255,255));
         rectangles[i].setOutlineThickness(2.0);
-
         x+=50;
-        x_text+= 50;
-        //cout<<"Sale del FOR"<<i<<endl;
     }
-
-    return rectangles;
-    
+    return rectangles; 
 }
 
 sf::RectangleShape createCurrentRectangle(pair <int,int> * current_card)
@@ -233,7 +187,6 @@ sf::Color getRectangleColor(int id_color)
     {
         color = sf::Color(255,255,0,120);   
     }
-
     return color;
 }
 
@@ -283,8 +236,19 @@ pair<int,int> getTurnCoords(int turn)
     }
     else if(turn == 3)
     {
-        coords.first = 10-20;
+        //coords.first = textSize;
+        coords.first = 50;
         coords.second = 300;
     }
     return coords;
+}
+
+string getArrowTurn(int turn)
+{
+    string arrow = "->";
+    if(turn == 3)
+    {
+        arrow = "<-";
+    }
+    return arrow;
 }
